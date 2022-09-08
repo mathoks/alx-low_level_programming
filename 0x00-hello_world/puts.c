2e4b96cfa224c07158fddfdcc7e0c7f4cfa0de4f
Mathew <mathoks2012@gmail.com>
2022-09-08 12:32:49 -0700

    puts

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints a string
+ * @str1: String to be output to the screen
+ *
+ * Description: Puts the string message to the screen
+ * put - Displays output to the screen 
+ * Return: Always 0 (success)
+ */
+int main()
+{
+char str1[55];
+ strcpy(str1,"Programming is like building a multilingual puzzle");
+
+puts(str1);
+
+return(0);
+
+}
