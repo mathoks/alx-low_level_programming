957d8af6b930d34bd790661b0d7e538eaf2e473f
Mathew <mathoks2012@gmail.com>
2022-09-08 13:31:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,12 @@
  * @str1: String to be output to the screen
  * strcpy - Copies variable
  * Description: Puts the string message to the screen
- * put - Displays output to the screen 
+ * put - Displays output to the screen
  * Return: Always 0 (success)
  */
 int main(void)
 {
-char str1[55];
+char str1[60];
 strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
 return (0);
