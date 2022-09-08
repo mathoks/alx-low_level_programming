a4cfa8e01bcd963ba2f4e7996234e4c310601e6c
Mathew <mathoks2012@gmail.com>
2022-09-08 14:39:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 #include <string.h>
 /**
  * main - Prints a string
- * @str: String to be output to screen 
+ * @str: String to be output to screen
  * Description: Puts the string message to the screen
- * put - Displays output to the screen
+ * puts - Displays output to the screen
  * Return: Always 0 (success)
  */
 int main(void)
 {
-char str[54] = "\"Programming is like building a multilingual puzzle";
+char str[53] = "\"Programming is like building a multilingual puzzle";
 puts(str);
 return (0);
 }
