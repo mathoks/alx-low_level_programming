fab47f34238c971f59b559ab8c922f7734efb7fc
Mathew <mathoks2012@gmail.com>
2022-09-08 14:52:30 -0700

    psh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints string
+ * Description: Prints all letters
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
