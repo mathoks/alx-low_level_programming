e1f0bbf74e003e67a4384612412b147c8317ee54
Mathew <mathoks2012@gmail.com>
2022-09-09 06:29:12 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
- char c;
- c='a';
- while (c <= 'z')
- {
- putchar(c);
- c++;
- }
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
 return (0);
 }
