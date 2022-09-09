d08a9c923923cc79c17cde69b4e26dbcae27963b
Mathew <mathoks2012@gmail.com>
2022-09-09 06:08:24 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase letters
+ * Description: prints letters in lower case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+ char c;
+ c='a';
+ while (c <= 'z')
+ {
+ putchar(c);
+ c++;
+ }
+return (0);
+}
