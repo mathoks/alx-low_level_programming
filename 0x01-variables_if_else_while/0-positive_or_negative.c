871c9c4ff47bb61a4b89ef4f8e741b9912b266d6
Mathew <mathoks2012@gmail.com>
2022-09-09 04:38:54 -0700

    loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,22 +4,19 @@
 
 /**
  * main - prints values
- * @n: interger values printed
- *
  * Description: checks if positive or nagative 
  * Return: 0 (success)
  */
-
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0)
+if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if(n == 0)
+else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
