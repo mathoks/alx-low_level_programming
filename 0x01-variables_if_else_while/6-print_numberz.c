1f307b69bbbc62d6ba85386a48d6414f326e3379
Mathew <mathoks2012@gmail.com>
2022-09-09 14:33:48 -0700

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- prints numbers
+ * Description: intergers only
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
