725ef42e9bce13c6aadd00810cb60712b7aa030d
Mathew <mathoks2012@gmail.com>
2022-09-13 09:16:01 -0700

    ghsgs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 /**
- * main - ghytrrt
- * Description: print alphabets
- * Return: 0;
+ * main - prints alphabet
+ * Description: cool
+ * Return: 0
  */
 int main(void)
 {
-int i = 'a';
-while (i <= 'z')
-{
-_putchar(i);
-i++;
-}
-_putchar('\n');
+print_alphabet();
 return (0);
 }
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - ghytrrt
+ * Description: print alphabets
+ * Return: 0;
+ */
+void print_alphabet(void)
+{
+int i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+
+}
