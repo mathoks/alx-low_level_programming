b387f77e9bf7aa86f935aa320379e9a65ca2fe23
Mathew <mathoks2012@gmail.com>
2022-09-13 15:54:48 -0700

    dgshdg

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  sign - hjfud
+ *  @n: variable
+ *  Definition: hdhhdh
+ *  Return: 1 or 0
+ */
+int sign (n)
+{
+if (n > 0)
+return (1);
+else if (n < 0)
+return (-1);
+else
+return (0);
+}
