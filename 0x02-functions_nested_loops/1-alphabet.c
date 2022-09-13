ce9ff630a5b83ef4e627c681eb1635ff439cc27c
Mathew <mathoks2012@gmail.com>
2022-09-13 08:53:32 -0700

    nhdj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - ghytrrt
+ * Description: print alphabets
+ * Return: 0;
+ */
+int main(void)
+{
+int i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+return (0);
+}
