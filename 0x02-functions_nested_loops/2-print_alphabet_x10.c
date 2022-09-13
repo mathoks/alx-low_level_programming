b2a33349fdeac39671a56f7991bb24efff7b0297
Mathew <mathoks2012@gmail.com>
2022-09-13 09:55:51 -0700

    hdgdkk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@ void print_alphabet_x10(void)
 {
 int i, j;
 
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
+if (i < 10)
 _putchar('\n');
 }
 }
