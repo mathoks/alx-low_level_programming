ac4b836d2301f68d6431da6fd0bdc32cfd3dd494
Mathew <mathoks2012@gmail.com>
2022-09-13 09:46:22 -0700

    hdgdff

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - ten lines
+ * Description: print ooo
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int i, j;
+
+for (i = 0; i <= 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
