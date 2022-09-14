8d18a5fce949293766e43d5e4270c184aa4ffb78
Mathew <mathoks2012@gmail.com>
2022-09-13 17:28:20 -0700

    gsfar

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - mhggga
+ * @c: ggiaiuu
+ * Description: hjkushg
+ * Return: 0
+ */
+int _abs(int c)
+{
+return (abs(c));
+}
+
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -9,12 +9,13 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
     return (0);
 }
