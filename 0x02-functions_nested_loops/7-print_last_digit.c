16ff7843863fdf4a067c6be8c4939db11059bc51
Mathew <mathoks2012@gmail.com>
2022-09-13 17:51:08 -0700

    hdffst

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - nmmshjjj
+ * @i: ghdhggdhh
+ * Description: hhsydgfdg
+ * Return: 0
+ */
+int print_last_digit(int i)
+{
+int num;
+num = _putchar((i % 10) + '0');
+return (num);
+}
