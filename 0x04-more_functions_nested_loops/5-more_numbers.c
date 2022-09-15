361e72b03be280129e8cfff559ed1f731983ffe5
Mathew <mathoks2012@gmail.com>
2022-09-15 07:40:09 -0700

    gdhf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar((i <= 9 ? ((i) + '0') : ((i / 10) % 10 + '0')));
-			if (i < 9)
-				_putchar(i % 10 + '0');
+			_putchar((j <= 9 ? ((j) + '0') : ((j / 10) % 10 + '0')));
+			if (j > 9)
+				_putchar(j % 10 + '0');
 		}
 	_putchar('\n');
 	}
