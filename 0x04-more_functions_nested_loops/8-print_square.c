f5d681cf0593b60d2d862f0d6361634a67d93359
Mathew <mathoks2012@gmail.com>
2022-09-15 13:00:21 -0700

    gdfd

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
@@ -17,6 +17,5 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-	if (i < size)
 	_putchar('\n');
 }
