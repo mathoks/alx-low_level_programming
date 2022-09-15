d3272030b58969ee6bb97e0a375343609eba9248
Mathew <mathoks2012@gmail.com>
2022-09-15 14:46:25 -0700

    dgfs

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,9 @@
 */
 int main(void)
 {
-int num = 1;
+int num;
 
-while (num++ < 100)
+for (num = 1; num <= 100; num++)
 {
 if ((num % 3 == 0) && (num % 5 == 0))
 {
@@ -23,14 +23,14 @@ printf("Fizz ");
 else if ((num % 5) == 0)
 {
 if (num != 100)
-{
 printf("Buzz ");
+else
+printf("Buzz");
 }
 else
 {
-printf("Buzz");
-}
-return (0);
+printf("%d ", num);
 }
 }
+printf("\n");
 }
