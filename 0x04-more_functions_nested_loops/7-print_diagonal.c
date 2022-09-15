7ebb709f3991ad1f14f4639c2281946c6bd60ae8
Mathew <mathoks2012@gmail.com>
2022-09-15 13:38:57 -0700

    nbbfhf

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,31 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * print_diagonal - hfgrtffd
- * @n: agiuuu
- * Description: hhgh
- * Return: sfdfghhf 0
- */
+* print_diagonal - this function prints a diagonal
+* @n: is a parameter
+* Return: nothing
+*/
 void print_diagonal(int n)
 {
-	int i, j;
+int len, space;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			if (i == j)
-				_putchar(92);
-			else if (n <= 0)
-				_putchar('\n');
-			else
-				_putchar(' ');
-		}
-		_putchar('\n');
-	}
-	_putchar('\n');
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+{
+for (space = 0; space < len; space++)
+{
+_putchar(' ');
+}
+
+_putchar('\\');
+
+if (len == (n - 1))
+{
+continue;
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
