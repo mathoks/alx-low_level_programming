530b8c168aa5e73d36f69829c54fade9ab22cb4b
Mathew <mathoks2012@gmail.com>
2022-09-19 16:05:04 -0700

    hhuu

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,8 +17,9 @@ void print_array(int *a, int n)
 		if (i != n - 1)
 		printf("%d, ", array[i]);
 		else if (n <= 0)
-			printf("%i\n", i);
+			printf("%d\n", array[i]);
 		else
 			printf("%d\n", array[i]);
 	}
+	printf("\n");
 }
