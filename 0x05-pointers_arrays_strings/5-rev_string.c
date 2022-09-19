f446b568bfd3b657457f9e0121563747b9654674
Mathew <mathoks2012@gmail.com>
2022-09-19 07:11:30 -0700

    hfgfdd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - prints
+ * @s: ggfhfh
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char *text = s;
+	char p = *s;
+
+	i = 0;
+	while (text[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(text[j]);
+	}
+	_putchar('\n');
+}
