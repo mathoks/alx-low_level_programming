709f569fea9fa18f73f22d44057a11e8c935b28a
Mathew <mathoks2012@gmail.com>
2022-09-19 15:04:01 -0700

    hhsgdh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "string.h"
+/**
+ * puts_half - hfhgb
+ * @str: gfgfhhf
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	char *key = str;
+	int i, j;
+
+	j = strlen(str);
+	for (i = 0; i <= j; i++)
+		if (j % 2 == 0)
+		{
+			if (i >= j / 2)
+			{
+				_putchar(key[i]);
+			}
+		}
+		else if (j % 2 != 0)
+		{
+			if (i >= (j - 1) / 2)
+			{
+				_putchar(key[i]);
+			}
+		}
+	_putchar('\n');
+}
