b8b4df6c7bf728861ce06c782cab902abed97c2c
Mathew <mathoks2012@gmail.com>
2022-09-26 05:46:25 -0700

    gfdf

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * _strchr - ghgg
+ * @s: string
+ * @c: hhhh
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	while (!*s = c)
+		if (!*s++)
+			return (0);
+	return (*s);
+}
