01f0490553bb40445bcc38dd74fc12767f610fea
Mathew <mathoks2012@gmail.com>
2022-10-12 15:51:04 -0700

    fgf

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - asdd
+ * @array: dtt
+ * @size: sdftf
+ * @cmp: dggg
+ * Return: hhg
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int r;
+	int y;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (0);
+	for (i = 0; i < size; i++)
+	{
+		r = array[i];
+		y = (cmp(r));
+		if (y != 0)
+			return (i);
+	}
+	return (i);
+}
