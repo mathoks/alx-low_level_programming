0d0725d1df646b1454942a4675e1dd8950116219
Mathew <mathoks2012@gmail.com>
2022-10-24 05:14:43 -0700

    fgfre

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+ * print_listint - print n
+ * @h: struct
+ * Return: n
+ */
+
+size_t print_listint(const listint_t *h)
+{
+const listint_t *ptr = h;
+size_t i;
+
+while (ptr != 0)
+{
+printf("%i\n", ptr->n);
+ptr = ptr->next;
+i++;
+}
+return (i);
+}
