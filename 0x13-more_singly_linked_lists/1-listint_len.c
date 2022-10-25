d6e3e85424b6cad559c8c77b5116473358380ef7
Mathew <mathoks2012@gmail.com>
2022-10-25 06:01:13 -0700

    add

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - lenght
+ * @h: struct list
+ * Return: i;
+ */
+size_t listint_len(const listint_t *h)
+{
+const listint_t *ptr = h;
+size_t i;
+while (ptr)
+{
+ptr = ptr->next;
+i++;
+}
+return (i);
+}
